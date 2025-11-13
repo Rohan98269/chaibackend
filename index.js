e7f23e4f5b4999f7abf821b13bd557210323f5de
Rohan Choudhary <choudharyrohan98269@gmail.com>
2025-11-13 14:38:17 +0530

    Third commit

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ require("dotenv").config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const jsonn ={
+"employee":{"name":"John", "age":30, "city":"New York"}
+}
 
 app.get('/', (req, res) => {
-  res.send('Hello World..!')
+  res.send('Hello World???..!')
 })
  app.get('/login',(req,res)=>{
     res.send("<h1>Welcome to our page</h1>")
  })
 
+ app.get('/jsonn',(req,res)=>{
+    res.send(jsonn)
+ })
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example apppp listening on port ${port}`)
 })
